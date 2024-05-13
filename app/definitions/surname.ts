2ee4bd12b65a70e00329cb41f159a8a10e3330a1
theo.selenskiy <theo.selenskiy@engineering.digital.dwp.gov.uk>
2024-05-13 15:56:54 +0100

    feat(mock ): add a simple mock service for getting addresses

--- app/definitions/surname.ts
+++ app/definitions/surname.ts
@@ -1,5 +1,9 @@
 import { field, validators as r } from '@dwp/govuk-casa';
 
 export default [
-  field('surname'),
+  field('surname').validators([
+    r.required.make({
+      errorMsg: 'Surname is required',
+    }),
+  ]),
 ]
